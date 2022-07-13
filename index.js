50e316c2e8a0f0cf97cb8d4846bccf82d63cfb68
motunrayoray <85764407+motunrayoray@users.noreply.github.com>
2022-07-13 19:17:12 +0100

    introduction to java script, Dom and events...

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('but-on').addEventListener('click',function(){
+    document.getElementById('page').innerText=choco
+})
+const tree = "glad to be here"
+const cloud = "feel at home!"
+const choco = "Welcome Choco!";
+window.addEventListener('load',function(){
+    document.getElementById('form-one').value=choco
+})
+window.addEventListener('load', function(){
+    document.getElementById('form-two').value= tree
+})
+window.addEventListener('load', function(){
+    document.getElementById('form-three').value= cloud
+})
